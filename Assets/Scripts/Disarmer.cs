692870891bc15d05301cf3d3c919ea07efa0bf40
AtlasOffMind <gameover.mg533@gmail.com>
2025-01-13 22:13:33 -0500

    3.9 Fixing Git problems

--- Assets/Scripts/Disarmer.cs
+++ Assets/Scripts/Disarmer.cs
@@ -12,8 +12,9 @@ public class Disarmer : Ability
     private Motion motion;
     private MazeCell currentCell;
     bool isActive;
-
     List<MazeCell> tempCell;
+
+    
     public override void Activate(Player parent)
     {
         player = parent;
@@ -22,11 +23,6 @@ public class Disarmer : Ability
         player.amount = amount;
         isActive = true;
         List<MazeCell> tempCell = new List<MazeCell>();
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 25e17007b4672106cb6948085f4342a9dd8c3e02
     }
 
     public override void Fast()
